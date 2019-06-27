13984c9ed13336afce7c92f7660f2beb79dde51a
Trevor Slaton <trevor.slaton@gmail.com>
2019-06-26 17:46:30 -0700

    Hook into built-in settings api + refactor

--- jupyterlab_favorites/favorites.py
+++ jupyterlab_favorites/favorites.py
@@ -8,45 +8,52 @@ from jupyterlab.commands import get_user_settings_dir
 class FavoritesHandler(IPythonHandler):
     def get(self):
         """
-        Get a list a favorites from data on disk.
+        Get default favorites and user favorites that are still valid on disk."
         """
         user_settings_dir = get_user_settings_dir()
-        favorites_settings_path = Path(user_settings_dir)/'jupyterlab-favorites'
-        favorites = []
+        favorites_settings_dir = Path(user_settings_dir)/'jupyterlab-favorites'
+        favorites_settings_file = favorites_settings_dir/'favorites.jupyterlab-settings'
+        response = {
+            'default': [],
+        }
+        # Parse saved user favorites into what's valid and what's invalid
         try:
-            favorites_settings_path = favorites_settings_path.resolve(strict=True)
-        except FileNotFoundError:
-            self.__write_default_favorites(favorites_settings_path)
-        else:
-            with open(favorites_settings_path, 'r') as f:
+            with open(favorites_settings_file, 'r') as f:
                 contents = f.read()
-                if not contents or not json.loads(contents):
-                    favorites = self.__write_default_favorites(favorites_settings_path)
-                else:
-                    for favorites_data in json.loads(contents).get('favorites', []):
-                        favorites.append(favorites_data)
-        self.write(json.dumps({ 'favorites': favorites }))
-
-    def __write_default_favorites(self, favorites_settings_path):
-        """
-        Establish default favorites if there are none otherwise.
-        """
-        favorites = []
-        home_dir = os.getenv('HOME', '')
-        if home_dir:
-            favorites.append({
+                for favorites_data in json.loads(contents).get('favorites', []):
+                    path = favorites_data['path']
+                    try:
+                        file = favorites_settings_file.resolve(strict=True)
+                        if file:
+                            if 'valid' not in response:
+                                response['valid'] = []
+                            response['valid'].append(favorites_data)
+                    except FileNotFoundError:
+                        if 'invalid' not in response:
+                            response['invalid'] = []
+                        response['invalid'].append(favorites_data)
+        except:
+            pass
+        # Write defaults
+        try:
+            home_dir = os.environ['HOME']
+            home_dir = Path(home_dir).resolve(strict=True)
+            response['default'].append({
                 'title': 'Home',
                 'iconClass': 'jp-HomeIcon',
-                'path': home_dir,
+                'path': str(home_dir),
             })
-        scratch_dir = os.getenv('SCRATCH', '')
-        if scratch_dir:
-            favorites.append({
+        except:
+            pass
+        try:
+            scratch_dir = os.environ['SCRATCH']
+            scratch_dir = Path(scratch_dir).resolve(strict=True)
+            response['default'].append({
                 'title': 'Scratch',
                 'iconClass': 'jp-FolderIcon',
-                'path': scratch_dir,
+                'path': str(scratch_dir),
             })
-        with open(favorites_settings_path, 'w') as f:
-            print('writing favorites: ', favorites)
-            json.dump({ 'favorites': favorites }, f)
-        return favorites
+        except:
+            pass
+
+        self.write(json.dumps(response))
--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,11 @@ import {
   IDocumentManager,
 } from '@jupyterlab/docmanager';
 
+// Can possibly use these to grab the appropriate icons by mimeType
+// import {
+//   defaultFileTypes,
+// } from '@jupyterlab/docregistry';
+
 import {
   IMainMenu
 } from '@jupyterlab/mainmenu';
@@ -34,6 +39,10 @@ import {
   ReactWidget,
 } from '@jupyterlab/apputils';
 
+import {
+  toArray,
+} from '@phosphor/algorithm';
+
 import React from 'react';
 
 namespace Favorites {
@@ -44,147 +53,111 @@ namespace Favorites {
     iconClass: string;
     path: string;
   }
-  export interface IState {
+
+  export interface IProps {
     favorites: Array<IItem>;
+  }
+
+  export interface IWidgetState {
+    default: Array<any>;
+    valid: Array<any> | undefined;
+    invalid: Array<any> | undefined;
   }
 }
 
-export class FavoritesComponent extends React.Component {
-  state: Favorites.IState = {
-    favorites: [],
-  };
+function FavoritesComponent(props: Favorites.IProps) {
+  return (
+    <div className="jp-Favorites">
+      <div className="jp-Favorites-header">
+        {"Favorites"}
+      </div>
+      <div className="jp-Favorites-container">
+        {props.favorites.map(f =>
+          <div className="jp-Favorites-item" title={f.path} key={`favorites-item-${f.path}`}>
+            <span className={`jp-MaterialIcon jp-Favorites-itemIcon ${f.iconClass}`}></span>
+            <span className="jp-Favorites-itemText">{f.title}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 
-  __get_favorites() {
-    // Request favorites from the server
-    const settings = ServerConnection.makeSettings();
-    ServerConnection.makeRequest(
-      `${settings.baseUrl}favorites`,
-      { method: 'GET' },
-      settings,
-    ).then(response => {
-      if (response.status !== 200) {
-        throw Error(response.statusText);
+class FavoritesWidget extends ReactWidget {
+  settingsID = `${Favorites.id}:favorites`;
+  settingsRegistry: ISettingRegistry;
+  favorites: Favorites.IWidgetState;
+
+  constructor(settingsRegistry: ISettingRegistry) {
+    super();
+    this.settingsRegistry = settingsRegistry;
+  }
+
+  async init() {
+    await this.getFavorites();
+    // Transform the plugin object to return different schema than the default.
+    this.settingsRegistry.transform(this.settingsID, {
+      fetch: plugin => {
+        const transformed = {
+          ...plugin,
+          schema: {
+            ...plugin.schema,
+            properties: {
+              ...plugin.schema.properties,
+              favorites: {
+                ...plugin.schema.properties.favorites,
+                default: this.favorites.default,
+              }
+            }
+          }
+        }
+        return transformed;
       }
-      return response.json();
-    }).then(data => {
-      console.log('received favorites: ', data);
-      this.updateFavorites(data.favorites || []);
-    }).catch(error => {
-      console.log(error);
     });
   }
 
-  componentDidMount() {
-    console.log('inside componentDidMount');
-    this.__get_favorites();
+  async getFavorites() {
+    const serverSettings = ServerConnection.makeSettings();
+    try {
+      const response = await ServerConnection.makeRequest(
+        `${serverSettings.baseUrl}favorites`,
+        { method: 'GET' },
+        serverSettings,
+      );
+      if (response.status !== 200) {
+        throw Error(response.statusText);
+      }
+      else {
+        this.favorites = await response.json();
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
   }
 
-  updateFavorites(favorites: Array<Favorites.IItem>) {
-    this.setState(() => {
-      return { favorites }
-    })
+  addFavorite(favorite: Favorites.IItem) {
+    console.log('adding favorite (just to client): ', favorite);
+    const valid = this.favorites.valid || [];
+    valid.push(favorite);
+    this.favorites.valid = valid;
+    console.log('forcing render with favorites: ', this.favorites.valid);
+    this.render();
   }
 
   render() {
-    return (
-      <div className="jp-Favorites">
-        <div className="jp-Favorites-header">
-          {"Favorites"}
-        </div>
-        <div className="jp-Favorites-container">
-          {this.state.favorites.map(f =>
-            <div className="jp-Favorites-item" title={f.path} key={`favorites-item-${f.path}`}>
-              <span className={`jp-MaterialIcon jp-Favorites-itemIcon ${f.iconClass}`}></span>
-              <span className="jp-Favorites-itemText">{f.title}</span>
-            </div>
-          )}
-        </div>
-      </div>
-    );
+    const favorites = this.favorites.valid || this.favorites.default;
+    return <FavoritesComponent favorites={favorites} />;
   }
 }
 
 /**
  * The command IDs used by the favorites plugin.
  */
-// namespace CommandIDs {
-//   export const addFavorite = 'jupyterlab-favorites:add-favorite';
-//   export const removeFavorite = 'jupyterlab-favorites:remove-favorite';
-// }
-
-// /**
-//  * Add a command to the right-click menu that adds a favorite.
-//  */
-// const addFavorite: JupyterFrontEndPlugin<void> = {
-//   activate: (app, factory) => {
-//     const { commands } = app;
-//     const { tracker } = factory;
-
-//     commands.addCommand(CommandIDs.addFavorite, {
-//       execute: () => {
-//         const widget = tracker.currentWidget;
-//         if (!widget) {
-//           return;
-//         }
-//         const path = widget.selectedItems().next().path;
-//         const settings = ServerConnection.makeSettings();
-//         // TODO: write path to file
-//         ServerConnection.makeRequest(
-//           `${settings.baseUrl}favorites`, {
-//             method: 'PATCH',
-//             body: JSON.stringify({ add: path }),
-//           },
-//           settings,
-//         ).then(response => {
-//           if (response.status !== 200) {
-//             throw Error(response.statusText);
-//           }
-//           // TODO: Get the Favorites to update
-//           return response.json();
-//         }).catch(error => {
-//           console.log(error);
-//         });
-//       },
-//       isVisible: () =>
-//         tracker.currentWidget &&
-//         toArray(tracker.currentWidget.selectedItems()).length === 1,
-//       iconClass: 'jp-MaterialIcon jp-LinkIcon',
-//       label: 'Copy Shareable Link',
-//     })
-//   },
-//   id: 'jupyterlab-favorites:add-favorite',
-//   requires: [IFileBrowserFactory, IFavorites],
-//   autoStart: true,
-// }
-
-// /**
-//  * Add a command to the right-click menu that adds a favorite.
-//  */
-// const removeFavorite: JupyterFrontEndPlugin<void> = {
-//   activate: (app, factory) => {
-//     const { commands } = app;
-//     const { tracker } = factory;
-
-//     commands.addCommand(CommandIDs.removeFavorite, {
-//       execute: () => {
-//         const widget = tracker.currentWidget;
-//         if (!widget) {
-//           return;
-//         }
-//         const path = encodeURI(widget.selectedItems().next().path);
-//         Clipboard.copyToSystem(URLExt.join(PageConfig.getTreeUrl(), path));
-//       },
-//       isVisible: () =>
-//         tracker.currentWidget &&
-//         toArray(tracker.currentWidget.selectedItems()).length === 1,
-//       iconClass: 'jp-MaterialIcon jp-LinkIcon',
-//       label: 'Copy Shareable Link',
-//     })
-//   },
-//   id: 'jupyterlab-favorites:add-favorite',
-//   requires: [IFileBrowserFactory],
-//   autoStart: true,
-// }
+namespace CommandIDs {
+  export const addFavorite = 'jupyterlab-favorites:add-favorite';
+  export const removeFavorite = 'jupyterlab-favorites:remove-favorite';
+}
 
 /**
  * Initialization data for the jupyterlab-favorites extension.
@@ -199,22 +172,23 @@ const favorites: JupyterFrontEndPlugin<void> = {
     IDocumentManager,
     IMainMenu,
   ],
-  activate: (
+  activate: async (
     app: JupyterFrontEnd,
     factory: IFileBrowserFactory,
     // read/write favorites here
-    settings: ISettingRegistry,
+    settingsRegistry: ISettingRegistry,
     // write recent files here
-    state: IStateDB,
+    stateDB: IStateDB,
     // open favorites with this?
-    manager: IDocumentManager,
+    docManager: IDocumentManager,
     // add command to open recents
     menu: IMainMenu
   ) => {
-    console.log('JupyterLab extension jupyterlab-favorites is activated (with React)!');
+    console.log('JupyterLab extension jupyterlab-favorites is activated!');
     const filebrowser = factory.defaultBrowser;
     const layout = filebrowser.layout as PanelLayout;
-    const favorites = ReactWidget.create(<FavoritesComponent />);
+    const favoritesWidget = new FavoritesWidget(settingsRegistry);
+    await favoritesWidget.init();
     let breadCrumbsIndex = 0;
     layout.widgets.forEach((widget, index) => {
       if (widget.node.className.includes('jp-BreadCrumbs')) {
@@ -222,19 +196,45 @@ const favorites: JupyterFrontEndPlugin<void> = {
       }
     })
     // Insert the Favorites widget just ahead of the BreadCrumbs
-    layout.insertWidget(breadCrumbsIndex, favorites);
-    console.log('settings: ', settings);
-    const loading = `${Favorites.id}:favorites`
-    console.log('loading recent: ', loading);
-    settings.load(loading)
-      .then(favoritesSettings => console.log('favoritesSettings: ', favoritesSettings));
-  }
+    layout.insertWidget(breadCrumbsIndex, favoritesWidget);
+    // Commands
+    const { commands } = app;
+    const { tracker } = factory;
+    commands.addCommand(CommandIDs.addFavorite, {
+      execute: () => {
+        const widget = tracker.currentWidget;
+        if (!widget) {
+          return;
+        }
+        const selectedItem = widget.selectedItems().next();
+        favoritesWidget.addFavorite({
+          title: selectedItem.name,
+          path: selectedItem.path,
+          iconClass: 'jp-FolderIcon',
+        });
+        // selectedItem.mimetype;
+      },
+      isVisible: () =>
+        tracker.currentWidget &&
+        toArray(tracker.currentWidget.selectedItems()).length === 1,
+      iconClass: 'jp-MaterialIcon jp-FavoritesIcon',
+      label: 'Add Favorite',
+    })
+    // matches all filebrowser items
+    const selectorItem = '.jp-DirListing-item[data-isdir]';
+    // // matches only non-directory items
+    // const selectorNotDir = '.jp-DirListing-item[data-isdir="false"]';
+    app.contextMenu.addItem({
+      command: CommandIDs.addFavorite,
+      selector: selectorItem,
+      rank: 3
+    });
+
+  },
 };
 
 const plugins: JupyterFrontEndPlugin<any>[] = [
   favorites,
-  // addFavorite,
-  // removeFavorite,
 ];
 
 export default plugins;
