b5e43b3751e4d3311b8365fca2a7da18751e4d0f
Labanya Mukhopadhyay <labanyam@hotmail.com>
2020-07-02 10:49:52 -0700

    update tests, add bash

--- tests/test_folder.py
+++ tests/test_folder.py
@@ -20,13 +20,16 @@ class TestAddfavs():
     def test_addfavs(self):
         self.driver.get("http://localhost:8889/lab/")
         self.driver.implicitly_wait(30)
+        WebDriverWait(self.driver, 200)
         
         # src_file = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[1]/div[2]/div[4]/ul/li[2]")
         # actions = ActionChains(self.driver)
         # actions.double_click(src_file).perform() #works
         tests = self.driver.find_element(By.XPATH, "//span[contains(text(),'tests')]")
+        WebDriverWait(self.driver, 200)
         actions = ActionChains(self.driver)
         WebDriverWait(self.driver, 200)
+        self.driver.implicitly_wait(65)
         actions.double_click(tests).perform()
         self.driver.implicitly_wait(15)
 
@@ -36,20 +39,27 @@ class TestAddfavs():
         self.driver.implicitly_wait(15)
         
         #test_folder_fav = self.driver.find_element(By.XPATH,"/html/body/div/div[3]/div[2]/div[1]/div[6]/div[2]/div/div/span") 
-        test_folder_fav = self.driver.find_element(By.XPATH,"/html/body/div/div[3]/div[2]/div[1]/div[6]/div[2]/div/div/span/svg")
+        test_folder_fav = self.driver.find_element(By.XPATH,"/html/body/div/div[3]/div[2]/div[1]/div[5]/div[2]/div/div/span")
+        
         #test_folder_fav = self.driver.find_elements_by_class_name("jp-Favorites-BreadCrumbs-Icon")
         actions = ActionChains(self.driver)
         actions.click(test_folder_fav).perform()
 
         # added_fav_text = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[1]/div[5]/div[2]/div/div[2]/div/span[2]")
-        added_fav_text = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[1]/div[6]/div[2]/div/div[2]/div")
+        added_fav_text = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[1]/div[5]/div[2]/div/div[2]/div/span[2]")
+        
         assert added_fav_text.text == "test_folder"
 
-        added_fav_elem = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[1]/div[6]/div[2]/div/div[2]/div")
+        added_fav_elem = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[1]/div[5]/div[2]/div/div[2]/div/span[2]")
         actions = ActionChains(self.driver)
         actions.context_click(added_fav_elem).perform()
         actions.send_keys(Keys.ARROW_DOWN).perform()
         actions.send_keys(Keys.ENTER).perform()
+
+        home_folder = self.driver.find_element(By.XPATH, "/html/body/div/div[3]/div[2]/div[1]/div[5]/div[3]/span[1]")
+        self.driver.implicitly_wait(15)
+        actions = ActionChains(self.driver)
+        actions.click(home_folder).perform() 
 
         
 
